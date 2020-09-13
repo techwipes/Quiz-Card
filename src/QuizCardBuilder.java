942323214f92f33b6f7aa6773fcb41e8cf380473
Mitya <techwipes@gmail.com>
2020-09-13 13:58:54 +0500

    first init, add variables and main

---
+++ src/QuizCardBuilder.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class QuizCardBuilder {
+
+    private JTextArea question;
+    private JTextArea answer;
+    private ArrayList <QuizCard> cardList;
+
+    public static void main(String[] args) {
+        QuizCardBuilder builder = new QuizCardBuilder();
+        builder.go();
+    }
+
+    public void go(){
+        //Creating GUI
+    }
+}
