1b4e930fbf6677a1e96e44c048ac6ceb75271368
Mitya <techwipes@gmail.com>
2020-09-13 15:03:17 +0500

    add variables

---
+++ src/QuizCardPlayer.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class QuizCardPlayer {
+
+    private JTextArea display;
+    private JTextArea answer;
+    private ArrayList <QuizCard> cardList;
+    private QuizCard currentCard;
+    private int currentCardIndex;
+    private JFrame frame;
+    private JButton nextButton;
+    private boolean isShowAnswer;
+
+}
